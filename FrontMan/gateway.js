73f463a0cfe6b2b6589cd203a06eb405cc2d5e53
Lokesh777 <lokeshdevgan777@gmail.com>
2022-05-05 10:39:10 +0530

    gateway complicated

---
+++ FrontMan/gateway.js
@@ -0,0 +1,8 @@
+document.querySelector(".sidebar").addEventListener("click", () => {
+    document.querySelector(".wrapper").classList.toggle("sPwrapper");
+  });
+  function proceedTopay(){
+    var box= document.getElementById("namelk").innerText="Payment success! Your Order is delivered within few minutes";
+         alert(box)}
+   
+
